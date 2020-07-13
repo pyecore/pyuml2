951416fe2e553012ff9e7bbdac7f82b56ac501cc
aranega <vincent.aranega@gmail.com>
2020-07-14 00:38:41 +0200

    Update setup.py, drop python 2.7 and 3.3 support

--- setup.py
+++ setup.py
@@ -34,10 +34,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
         'Topic :: Software Development',
